2f30bd062c80e8a70a629eb7ae2969b9c6ee0b8f
amitsrivastava-cchmc <amit19832@gmail.com>
2024-12-02 10:43:38 -0500

    sim_effects_and_pheno_pooled_data_run.R

--- sim_effects_and_pheno_pooled_data_run.R
+++ sim_effects_and_pheno_pooled_data_run.R
@@ -13,5 +13,4 @@ cat(dir_name, nvar1, nvar2, nvar3, var_m, var_f, "\n");
 
 source("/data/predataSamit/simulation_with_poe_and_mf_cor/sim_effects_and_pheno_pooled_data.R");
 
-sim_eff_pheno(direc = dir_name, phen_rep = 100, var_ids = "/data/predataSamit/simulation_with_poe_and_mf_cor/gcta_simulation_pooled_data_maf0.000001/pooled_data_comm_maf0.000001_variants.txt", nvar = c(nvar1, nvar2, nvar3), var_m = var_m, var_f = var_f);
-
+sim_eff_pheno(direc = dir_name, phen_rep = 100, var_ids = "/data/predataSamit/simulation_with_poe_and_mf_cor/gcta_simulation_pooled_data/pooled_data_comm_variants.txt", nvar = c(nvar1, nvar2, nvar3), var_m = var_m, var_f = var_f);
